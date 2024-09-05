a598a588eb839062a8c57b2ca16e0d33c192589c
Milena Ceglia Mihajilovic <ema.mihajilovic@gmail.com>
2024-09-05 14:02:45 +0000

    Install requirements for deploying the project

--- django_project/settings.py
+++ django_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dzj)%do0k%vi-(2w5y^w!o-qp)vd6ve^29pg2#qb*zwgq&e8ag'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-emamiha-foodomania-lle62yzgd32.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-emamiha-foodomania-lle62yzgd32.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
 # Application definition
