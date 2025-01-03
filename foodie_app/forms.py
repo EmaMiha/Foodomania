4f3113ec0cb165e5071218aa277b8f79a7dc8796
Milena Ceglia Mihajilovic <ema.mihajilovic@gmail.com>
2025-01-03 10:33:18 +0000

    Add readme.md

--- foodie_app/forms.py
+++ foodie_app/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from django.forms import inlineformset_factory
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from .models import Recipe,Diet,Category,Comment,Ingredient
+from .models import Recipe, Diet, Category, Comment, Ingredient
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -13,36 +13,38 @@ class CustomUserCreationForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
+
 class CustomAuthenticationForm(AuthenticationForm):
     username = forms.CharField(max_length=150)
     password = forms.CharField(widget=forms.PasswordInput)
 
+
 class RecipeForm(forms.ModelForm):
-    
-        diet = forms.ModelChoiceField(queryset=Diet.objects.all(), empty_label="Select Diet",required=True)
-        categories = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label="Select Category",required=True)
-        class Meta:
-            model = Recipe
-            fields = ['title', 'diet', 'categories','image']  
 
+    diet = forms.ModelChoiceField(
+        queryset=Diet.objects.all(), empty_label="Select Diet", required=True)
+    categories = forms.ModelChoiceField(
+        queryset=Category.objects.all(), empty_label="Select Category", 
+        required=True)
 
-
-        
+    class Meta:
+        model = Recipe
+        fields = ['title', 'diet', 'categories', 'image']
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
-        model=Comment
-        fields=['content']
+        model = Comment
+        fields = ['content']
 
 
 class CategoryForm(forms.ModelForm):
     class Meta:
-        model=Category
-        fields=['name','description']
+        model = Category
+        fields = ['name', 'description']
 
 
 class DietForm(forms.ModelForm):
     class Meta:
-        model=Diet
-        fields=['name','description']
+        model = Diet
+        fields = ['name', 'description']
