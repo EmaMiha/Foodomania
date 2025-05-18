57c2b05633c300fc3bae2c169f89d80e784fa663
Milena Ceglia Mihajilovic <ema.mihajilovic@gmail.com>
2025-05-18 18:57:16 +0200

    Update static files

--- django_project/settings.py
+++ django_project/settings.py
@@ -16,9 +16,9 @@ import dj_database_url
 # if os.path.isfile('env.py'):
 #     import env
 
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-# load_dotenv()
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -148,7 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'foodie_app/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'foodie_app/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
